d1459b89420388848cd6b3f49044194c87608b8b
Javiornitier <javilob97@gmail.com>
2016-12-20 19:57:50 +0100

    #4 Movimiento Futbol 2
    Al igual que el anterior no me deja insertar el script, creo que es
    porque el otro script "Nave" no está bien.

---
+++ Assets/Scripts/Jugador2.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jugador2 : MonoBehaviour {
+
+
+	public float velocidad = 100f;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.D)) {
+			MovimientoDerecha ();
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			MovimientoIzquierda ();
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			MovimientoArriba ();
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			MovimientoAbajo ();
+		}
+	}
+	void MovimientoDerecha(){
+		rb.velocity = new Vector2(velocidad, rb.velocity.y);
+		this.transform.localScale = new Vector3(1, 1, 1);
+	}
+
+	void MovimientoIzquierda() {
+		rb.velocity = new Vector2(-velocidad, rb.velocity.y);
+		this.transform.localScale = new Vector3(-1, 1, 1);
+	}
+
+	void MovimientoArriba(){
+		rb.velocity = new Vector2(velocidad, rb.velocity.x);
+		this.transform.localScale = new Vector3(1, 1, 1);
+	}
+
+	void MovimientoAbajo() {
+		rb.velocity = new Vector2(-velocidad, rb.velocity.x);
+		this.transform.localScale = new Vector3(-1, 1, 1);
+	}
+}
