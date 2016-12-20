e5ce7ba734457dce9ef549a921493104b866cc5c
Javiornitier <javilob97@gmail.com>
2016-12-20 19:55:31 +0100

    #3 Movimiento futbol
    No ne funciona el script (no me deja insertar el script) y no tengo muy
    claro el por qué.

---
+++ Assets/Scripts/Jugador1.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jugador1 : MonoBehaviour {
+
+
+	public float velocidad = 100f;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			MovimientoDerecha ();
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			MovimientoIzquierda ();
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			MovimientoArriba ();
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			MovimientoAbajo ();
+		}
+	}
+	void MovimientoDerecha(){
+			rb.velocity = new Vector2(velocidad, rb.velocity.y);
+			this.transform.localScale = new Vector3(1, 1, 1);
+		}
+
+	void MovimientoIzquierda() {
+			rb.velocity = new Vector2(-velocidad, rb.velocity.y);
+			this.transform.localScale = new Vector3(-1, 1, 1);
+		}
+			
+	void MovimientoArriba(){
+			rb.velocity = new Vector2(velocidad, rb.velocity.x);
+			this.transform.localScale = new Vector3(1, 1, 1);
+		}
+
+	void MovimientoAbajo() {
+			rb.velocity = new Vector2(-velocidad, rb.velocity.x);
+			this.transform.localScale = new Vector3(-1, 1, 1);
+		}
+	}
